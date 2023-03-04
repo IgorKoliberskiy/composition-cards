6f63e1ee98cd04fd0eff8225f8e423f5d484edcd
Igor Koliberskiy <kolibry60rus@mail.ru>
2023-03-04 19:50:23 +0300

    HW composition-cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Cards from './components/Cards';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cards>
+        <div className="card">
+          <div className="card-body">
+            <div className="card-img-top">
+              <p class="card-img-top__text">Image cap</p>
+            </div>
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#top" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </Cards>
+      <Cards>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#top" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </Cards>
     </div>
   );
 }
